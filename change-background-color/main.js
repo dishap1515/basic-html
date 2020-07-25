f591f34758daa29d6f21fd7edbe4e32bde2a9e44
Disha Patel <dishapatelvadodara15@gmail.com>
2020-07-26 00:09:01 +0530

    Color change on click

---
+++ change-background-color/main.js
@@ -0,0 +1,10 @@
+var colors = ["pink", "lightblue", "green", "maroon", "grey", "yellow"];
+    var colorIndex = 0;
+    function changeColor() {
+        var col = document.getElementById("body");
+        if( colorIndex >= colors.length ) {
+            colorIndex = 0;
+        }
+        col.style.backgroundColor = colors[colorIndex];
+        colorIndex++;
+    }
