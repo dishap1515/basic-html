56969ce2328d48a7a8c3823eb157b4c7aa55e7b8
Disha Patel <dishapatelvadodara15@gmail.com>
2020-07-27 00:00:36 +0530

    Enabled Hex color-code label

--- change-background-color/main.js
+++ change-background-color/main.js
@@ -1,10 +1,13 @@
-var colors = ["pink", "lightblue", "green", "maroon", "grey", "yellow"];
+var colors = ["pink", "lightblue", "limegreen", "coral", "grey", "olive"];
+var colorCode = ["#FFCOCB", "#ADD8E6", "#32CD32", "#FF7F50", "#808080", "#808000"];
     var colorIndex = 0;
     function changeColor() {
         var col = document.getElementById("body");
+        var a = document.getElementById("label");
         if( colorIndex >= colors.length ) {
             colorIndex = 0;
         }
         col.style.backgroundColor = colors[colorIndex];
+        a.innerHTML = colorCode[colorIndex];
         colorIndex++;
     }
