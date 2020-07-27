a4eb138937c802948c24206f47b990b66842616b
Disha Patel <dishapatelvadodara15@gmail.com>
2020-07-27 21:57:40 +0530

    Randomize Colors

--- change-background-color/main.js
+++ change-background-color/main.js
@@ -1,13 +1,14 @@
-var colors = ["pink", "lightblue", "limegreen", "coral", "grey", "olive"];
-var colorCode = ["#FFCOCB", "#ADD8E6", "#32CD32", "#FF7F50", "#808080", "#808000"];
-    var colorIndex = 0;
-    function changeColor() {
-        var col = document.getElementById("body");
-        var a = document.getElementById("label");
-        if( colorIndex >= colors.length ) {
-            colorIndex = 0;
-        }
-        col.style.backgroundColor = colors[colorIndex];
-        a.innerHTML = colorCode[colorIndex];
-        colorIndex++;
+function getNewColor() {
+    var symbols, colors;
+    symbols = "0123456ABCDEF";
+
+    color = "#";
+    for(var i = 0; i < 6; i++){
+        color = color + symbols[Math.floor(Math.random() * 16)];
+
     }
+    document.body.style.background = color;
+    document.getElementById("hex").innerHTML = color;
+} 
+
+    
